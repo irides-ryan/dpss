442fe0d65a3e45729835b229a141b678e29c7dba
ryan <mr.iridescent.rsy@hotmail.com>
2018-10-24 10:16:58 +0800

    Clean up: use 'dpss' as application name.

--- client/main.cpp
+++ client/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
     DApplication::loadDXcbPlugin();
     DApplication app(argc, argv);
     app.setAttribute(Qt::AA_UseHighDpiPixmaps);
-    const QString socket_path(QString("shadowsocks-client_%1").arg(getuid()));
+    const QString socket_path(QString("dpss_%1").arg(getuid()));
     if (app.setSingleInstance(socket_path)) {
         signal(SIGINT, onSignalRecv);
         signal(SIGTERM, onSignalRecv);
@@ -33,15 +33,15 @@ int main(int argc, char *argv[]) {
         const QString descriptionText = QApplication::tr(
                     "If you want to keep a secret, you must also hide it from yourself.");
 
-        const QString acknowledgementLink = "https://github.com/PikachuHy/shadowsocks-client";
+        const QString acknowledgementLink = "https://github.com/irides-ryan/dpss";
 
-        app.setOrganizationName("pikachu");
-        app.setApplicationName("shadowsocks-client");
-        app.setApplicationDisplayName(QObject::tr("Shadowsocks Client"));
-        app.setApplicationVersion("1.1.0");
+        app.setOrganizationName("irides");
+        app.setApplicationName("dpss");
+        app.setApplicationDisplayName(QObject::tr("ss for deepin"));
+        app.setApplicationVersion("1.3.0");
 
         app.setProductIcon(QIcon(Utils::getIconQrcPath("ssw128.svg")));
-        app.setProductName(QApplication::tr("Shadowsocks Client"));
+        app.setProductName(QApplication::tr("dpss"));
         app.setApplicationDescription(descriptionText);
         app.setApplicationAcknowledgementPage(acknowledgementLink);
 
