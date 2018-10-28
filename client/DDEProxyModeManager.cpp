5e3b87d7dde22db538a57e842e57fb94bb3de317
ryan <mr.iridescent.rsy@hotmail.com>
2018-10-28 22:22:15 +0800

    Clean up: clean the DDEProxyModeManager.

--- client/DDEProxyModeManager.cpp
+++ client/DDEProxyModeManager.cpp
@@ -1,12 +1,33 @@
 #include "DDEProxyModeManager.h"
 
+DDEProxyModeManager::DDEProxyModeManager(QObject *parent)
+    : SystemProxyModeManager(parent),
+      networkInter("com.deepin.daemon.Network",
+                   "/com/deepin/daemon/Network",
+                   QDBusConnection::sessionBus(),
+                   this)
+{}
+
+void DDEProxyModeManager::setProxyMethod(QString proxyMethod) {
+    auto watcher = new QDBusPendingCallWatcher(
+                networkInter.SetProxyMethod(proxyMethod),
+                this);
+    QObject::connect(watcher, &QDBusPendingCallWatcher::finished, [=] {
+        qDebug() << "success to set proxy method " << proxyMethod;
+    });
+    connect(watcher, &QDBusPendingCallWatcher::finished,
+            watcher, &QDBusPendingCallWatcher::deleteLater);
+}
+
 void DDEProxyModeManager::switchToNone() {
     setProxyMethod("none");
 }
 
 void DDEProxyModeManager::switchToAuto(QString pacURI) {
-    auto w = new QDBusPendingCallWatcher(networkInter.SetAutoProxy(pacURI), this);
-    QObject::connect(w, &QDBusPendingCallWatcher::finished, [=]() {
+    auto watcher = new QDBusPendingCallWatcher(
+                networkInter.SetAutoProxy(pacURI),
+                this);
+    QObject::connect(watcher, &QDBusPendingCallWatcher::finished, [=]() {
         qDebug() << "set pac URI " << pacURI;
         setProxyMethod("auto");
     });
@@ -16,23 +37,13 @@ void DDEProxyModeManager::switchToManual(QString localAddress, int _port) {
     QString type = "socks";
     QString addr = localAddress;
     QString port = QString::number(_port);
-    auto w = new QDBusPendingCallWatcher(networkInter.SetProxy(type, addr, port), this);
-    QObject::connect(w, &QDBusPendingCallWatcher::finished, [=] {
+    auto watcher = new QDBusPendingCallWatcher(
+                networkInter.SetProxy(type, addr, port),
+                this);
+    QObject::connect(watcher, &QDBusPendingCallWatcher::finished, [=] {
         qDebug() << "set proxy" << type << addr << port;
         setProxyMethod("manual");
     });
-    connect(w, &QDBusPendingCallWatcher::finished, w, &QDBusPendingCallWatcher::deleteLater);
-}
-
-DDEProxyModeManager::DDEProxyModeManager(QObject *parent)
-    : SystemProxyModeManager(parent),
-      networkInter("com.deepin.daemon.Network", "/com/deepin/daemon/Network", QDBusConnection::sessionBus(),
-                   this) {}
-
-void DDEProxyModeManager::setProxyMethod(QString proxyMethod) {
-    QDBusPendingCallWatcher *w = new QDBusPendingCallWatcher(networkInter.SetProxyMethod(proxyMethod), this);
-    QObject::connect(w, &QDBusPendingCallWatcher::finished, [=] {
-        qDebug() << "success to set proxy method " << proxyMethod;
-    });
-    connect(w, &QDBusPendingCallWatcher::finished, w, &QDBusPendingCallWatcher::deleteLater);
+    connect(watcher, &QDBusPendingCallWatcher::finished,
+            watcher, &QDBusPendingCallWatcher::deleteLater);
 }
