44e53063523a277e3d6a188a3966051f6c91e022
ryan <mr.iridescent.rsy@hotmail.com>
2018-11-22 19:11:37 +0800

    Add: complete the fromJson and toJson.

--- client/config/Configuration.cc
+++ client/config/Configuration.cc
@@ -14,7 +14,15 @@ void Configuration::fromJson(QJsonObject &json) {
     m_servers = Server::fromJson(_servers);
     auto _proxy = json["proxy"].toObject();
     ((Proxy &)m_proxy).fromJson(_proxy);
+
     m_shareOverLan = json["shareOverLan"].toBool();
+    m_localPort = static_cast<uint16_t>(json["localPort"].toInt());
+
+    m_index = json["index"].toInt();
+    m_enable = json["enable"].toBool();
+    m_global = json["global"].toBool();
+    m_autoStart = json["autoStart"].toBool();
+    m_autoCheckUpdate = json["autoCheckUpdate"].toBool();
   } else {
     *this = Configuration();
   }
@@ -22,7 +30,17 @@ void Configuration::fromJson(QJsonObject &json) {
 
 QJsonObject Configuration::toJson() {
   QJsonObject json;
-  // TODO
+  json.insert("configs", ((Server*)&m_servers)->toJson());
+  json.insert("proxy", ((Proxy*)&m_proxy)->toJson());
+
+  json.insert("localPort", m_localPort);
+  json.insert("shareOverLan", m_shareOverLan);
+
+  json.insert("enable", m_enable);
+  json.insert("index", m_index);
+  json.insert("global", m_global);
+  json.insert("autoStart", m_autoStart);
+  json.insert("autoCheckUpdate", m_autoCheckUpdate);
   return json;
 }
 
