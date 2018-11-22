e5eac3ca42093253938aefe7e5679f63752901f1
ryan <mr.iridescent.rsy@hotmail.com>
2018-11-22 14:42:11 +0800

    Add: GConfig::Load & GConfig::Save.

--- client/GConfig.cc
+++ client/GConfig.cc
@@ -1,6 +1,9 @@
+#include <QtCore/QJsonDocument>
 #include "GConfig.h"
+#include "utils.h"
 
-GConfig::GConfig() {
+GConfig::GConfig()
+  : m_configPath(QDir(Utils::configPath()).filePath(CONFIG_FILE)) {
 
 }
 
@@ -9,17 +12,64 @@ GConfig::~GConfig() {
 }
 
 void GConfig::Load(QString const &path) {
+  QFile file(path);
+  QFileInfo fileInfo(file);
+  QDir dir = fileInfo.absoluteDir();
 
+  if (!dir.exists()) {
+    dir.mkpath(dir.absolutePath());
+  }
+
+  // file not exist
+  if (!file.exists()) {
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+      // TODO let someone know.
+      return;
+    } else {
+      // write a default value
+      QJsonDocument document;
+      document.setObject(m_config.toJson());
+      file.write(document.toJson());
+      file.flush();
+      file.close();
+    }
+  } else {
+    // file exists
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    auto json = QJsonDocument::fromJson(file.readAll()).object();
+    m_config.fromJson(json);
+
+    file.close();
+  }
 }
 
 void GConfig::Save(QString const &path) {
+  QFile file(path);
+  QFileInfo fileInfo(file);
+  QDir dir = fileInfo.absoluteDir();
 
+  if (!dir.exists()) {
+    dir.mkpath(dir.absolutePath());
+  }
+
+  if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    // TODO let someone know.
+    return;
+  } else {
+    // write a default value
+    QJsonDocument document;
+    document.setObject(m_config.toJson());
+    file.write(document.toJson());
+    file.flush();
+    file.close();
+  }
 }
 
 void GConfig::Load() {
-
+  Load(m_configPath);
 }
 
 void GConfig::Save() {
-
+  Save(m_configPath);
 }
--- client/GConfig.h
+++ client/GConfig.h
@@ -1,19 +1,24 @@
 #pragma once
 
+#include <QObject>
+#include <QDir>
 #include "config/Configuration.h"
 
-class GConfig
+class GConfig : public QObject
 {
+Q_OBJECT
+
 private:
   const char* CONFIG_FILE = "gui-config.json";
 
-  config::Configuration configuration;
+  config::Configuration m_config;
+  const QString m_configPath;
 
   void Load(QString const &path);
   void Save(QString const &path);
 public:
   GConfig();
-  ~GConfig();
+  ~GConfig() override;
 
   void Load();
   void Save();
